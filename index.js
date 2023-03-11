f5afe919fbbff5d2306304675c6c22ab36cab6d7
richard <richard.landaeta@gmail.com>
2023-03-10 22:40:36 -0600

    all passed test

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+  let thankYouArray = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    let thankYouMsg = `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`;
+    thankYouArray.push(thankYouMsg);
+  }
+  return thankYouArray;
+}
+
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+  }
+}
